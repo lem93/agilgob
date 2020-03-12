9b7125b41dc38c412160d31d63c6e0a4fcc6b6bd
Luis Espinosa <lem.espinosa.m@gmail.com>
2020-03-12 16:20:53 -0700

    database migrations and seeding

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,12 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->string('nick')->primary();
+            $table->string('nombre');
+            $table->string('apellidos');
+            $table->string('password')->required();
+            $table->integer('rol');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -2,15 +2,16 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+use App\User;
+class UserSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-      $this->call(UserSeeder::class);
+      factory(User::class, 100)->create();
     }
 }
